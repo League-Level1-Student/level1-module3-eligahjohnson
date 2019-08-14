c47447dfd911b69aef472c94fe25e126eae9cec3
ilabstaff <ilabstaff@172.16.116.79>
2019-08-13 19:14:40 -0700

    Backpack

--- src/_02_backpack/BackpackRunner.java
+++ src/_02_backpack/BackpackRunner.java
@@ -2,6 +2,14 @@ package _02_backpack;
 
 public class BackpackRunner {
 	public static void main(String[] args) {
-		new Backpack().packAndCheck();
+		
+		Backpack drake = new Backpack();
+		Pencil lilnas = new Pencil ();
+		Ruler sixnine = new Ruler ();
+		Textbook kodakb = new Textbook ();
+		drake.putInBackpack(lilnas);
+		drake.putInBackpack(sixnine);
+		drake.putInBackpack(kodakb);
+		drake.goToSchool();
 	}
 }
